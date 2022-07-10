100ae80ab5dbc361bbe2a281f713e3960061cd3d
Oscar Bernal <odbg2013@gmail.com>
2022-07-09 19:20:33 -0500

    api mini phrases completed

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function getRandomQuote() {
 }
 
 async function renderQuote() {
-    const quote = await getRandomQuote();
-    text.innerText = quote;
+  const quote = await getRandomQuote();
+  text.innerText = quote;
+  changeFontFamily();
+  changeColor();
+}
+
+function randomItem(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+function changeFontFamily() {
+  let arrFontFamily = [
+    'Verdana, sans-serif',
+    'Courier New, monospace',
+    'Gill Sans, calibri',
+    'Franklin Gothic Medium, sans-serif',
+    'Lucida Sans, sans-serif',
+    'Times New Roman, serif',
+    'Georgia, serif',
+  ];
+  let randomFontFamily = randomItem(arrFontFamily);
+
+  text.style.fontFamily = randomFontFamily;
+}
+
+function changeColor() {
+  let arrColor = ['antiquewhite', 'blanchedalmond', 'aliceblue', 'wheat', 'tan'];
+  let randomColor = randomItem(arrColor);
+
+  textContainer.style = `background-color: ${randomColor}`;
 }
 
 renderQuote();
